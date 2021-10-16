b3da60e8abc79ad570ba254faeaf2b870690896e
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-16 20:54:52 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,27 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testTrue() {
+        assertEquals(BooleanToString.convert(true), "true");
+    }
+
+    @Test
+    public void testFalse() {
+        assertEquals(BooleanToString.convert(false), "false");
+    }
+
+    private static class BooleanToString {
+        public static String convert(boolean b) {
+            String value = Boolean.toString(b);
+            if (value == "true") {
+                return "true";
+            } else {
+                return "false";
+            }
+        }
 
     }
 }
